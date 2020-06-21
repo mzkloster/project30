4d6dcb725a7eff58eba869d9343035769a6bef16
Martin Kloster <martin.kloster91@gmail.com>
2020-06-21 18:30:09 +0200

    gardinlosning som funker

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 $('.gift-curtain').on("click", function(){
     // $(this).children('#curtain-left').fadeOut();
-    $(this).children('#curtain-left').animate({
-        left: '0%',
-    });
+    $(this).children('#curtain-left').slideUp(2000);
+    // $(this).children('#curtain-left').animate({left: '0px'}, 'slow');
   });
