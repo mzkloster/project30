4fb8e07f4df215d71df2bb4f83ff679665342594
Martin Kloster <martin.kloster91@gmail.com>
2020-06-21 22:12:11 +0200

    fungerende gardiner

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $('.gift-curtain').on("click", function(){
-    // $(this).children('#curtain-left').fadeOut();
-    $(this).children('#curtain-left').slideUp(2000);
-    // $(this).children('#curtain-left').animate({left: '0px'}, 'slow');
-  });
+$(this).children('#curtains').slideUp(2000);
+});
+
