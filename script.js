d21a900d0c45c336bbc9189088bf9685ef551a78
Martin Kloster <martin.kloster91@gmail.com>
2020-06-24 20:28:48 +0200

    card og curtain er ferdig

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-$('.gift-curtain.gift-laptop, #openLaptopGift').on("click", function(){
-$('.gift-laptop').children('#curtains').slideToggle(2000);
+$('.gift-curtain.gift-laptop, #openLaptopGift').on('click', function(){
+    $('.gift-laptop').children('#curtains').slideToggle(3000);
+
+    if ($('.card-laptop').css("display") === 'none'){
+        $('.card-laptop').fadeTo(3000, 0.25).fadeTo(1000, 1);
+    }
 });
+
+$('.card-header').on('click', function(){        
+    $(this).siblings('.card-body').toggle();
+});
+
+$('.card-header').hover(function(){
+    $(this).css("background-color", "#495867");
+    }, function(){
+    $(this).css("background-color", "#577399");
+  });
+
+
 
