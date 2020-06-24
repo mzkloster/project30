eaf57aafb26c137154374ae08f090407d2f908a7
Martin Kloster <martin.kloster91@gmail.com>
2020-06-24 22:00:07 +0200

    refakturering

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # project30
 color palette: https://coolors.co/495867-577399-bdd5ea-f7f7ff-fe5f55
+mørkest blå: #495867 (hover background-color, text color)
+nest mørkest blå: #577399 (cardheader background-color) 
+lyseblå: #BDD5EA (jumbotron background-color)
+hvit: #F7F7FF (cardbody background-color)
+orange: #FE5F55 (h1 text color)
--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-$('.gift-curtain.gift-laptop, #openLaptopGift').on('click', function(){
-    $('.gift-laptop').children('#curtains').slideToggle(3000);
+$('.card-header').on('click', function(){        
+    $(this).siblings('.card-body').slideToggle();
+});
 
-    if ($('.card-laptop').css("display") === 'none'){
-        $('.card-laptop').fadeTo(3000, 0.25).fadeTo(1000, 1);
+
+//   LAPTOP
+  $('#gift-curtain-laptop, #open-laptop-gift-button').on('click', function(){
+    $('#gift-curtain-laptop').children('#curtains').slideToggle(3000);
+
+    if ($('#card-laptop').css("display") === 'none'){
+        $('#card-laptop').fadeTo(2500, 0.25).fadeTo(1000, 1);
     }
 });
-
-$('.card-header').on('click', function(){        
-    $(this).siblings('.card-body').toggle();
-});
-
-$('.card-header').hover(function(){
-    $(this).css("background-color", "#495867");
-    }, function(){
-    $(this).css("background-color", "#577399");
-  });
-
-
 
