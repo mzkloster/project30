9d387848b8125caff166078d3073218e84a4a207
Martin Kloster <martin.kloster91@gmail.com>
2020-07-17 19:01:28 +0200

    bugfix - test1

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ $('.card-header').on('click', function(){
     $(this).siblings('.card-body').slideToggle();
 });
 
+var bg = $('body');
 
-//   LAPTOP
+function resizeBackground() {
+    bg.height( $(window).height() + 60);
+}
+
+$(window).resize(resizeBackground);
+resizeBackground();
+
+
+//   All gift curtains
 $('.gift-alternative').children('.gift-curtain, .open-gift-button').on('click', function(){
     $(this).parent().find('#curtains').slideToggle(3000);
 
